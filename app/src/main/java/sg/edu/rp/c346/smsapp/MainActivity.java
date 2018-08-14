48aaf7354de0462df2e652570365360ffa07c6d0
CHUA MING HAN <17010265@rp.edu.sg>
2018-08-14 20:21:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/smsapp/MainActivity.java
@@ -0,0 +1,69 @@
+package sg.edu.rp.c346.smsapp;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTo;
+    EditText etTo;
+    TextView tvCont;
+    EditText etCont;
+    Button btnSend;
+    BroadcastReceiver br = new MessagerReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        this.registerReceiver(br, filter);
+
+        checkPermission();
+
+        tvTo = findViewById(R.id.textViewTo);
+        etTo = findViewById(R.id.editTextTo);
+        tvCont = findViewById(R.id.textViewContent);
+        etCont = findViewById(R.id.editTextContent);
+        btnSend = findViewById(R.id.buttonSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(etTo.getText().toString(), null, etCont.getText().toString(), null, null);
+
+            }
+        });
+    }
+    private void checkPermission() {
+        int permissionSendSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+        int permissionRecvSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS);
+        if (permissionSendSMS != PackageManager.PERMISSION_GRANTED &&
+                permissionRecvSMS != PackageManager.PERMISSION_GRANTED) {
+            String[] permissionNeeded = new String[]{Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_SMS};
+            ActivityCompat.requestPermissions(this, permissionNeeded, 1);
+        }
+    }
+    }
+
